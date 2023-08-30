8b234b14ade8026eef6a14c325b3c1ac7b97b596
luminousol <luminousolding@gmail.com>
2023-08-30 23:31:51 +0900

    comment: 주석 추가

--- server/src/main/java/com/codestates/server/global/dto/MultiResponseDto.java
+++ server/src/main/java/com/codestates/server/global/dto/MultiResponseDto.java
@@ -11,12 +11,14 @@ import java.util.List;
 @Getter
 public class MultiResponseDto<T> {
 
-    private List<T> data;
-    private PageInfo pageInfo;
+    private List<T> data;   // 응답 데이터 목록 담는 필드
+    private PageInfo pageInfo;  // 페이지 정보 담는 필드
+
     public MultiResponseDto(List<T> data, Page page) {
 
         this.data = data;
 
+        // 페이지 번호 0부터 시작하기 때문에 1부터 시작하는 걸로 변경
         this.pageInfo = new PageInfo(page.getNumber() + 1,
                 page.getSize(), page.getTotalElements(), page.getTotalPages());
 
