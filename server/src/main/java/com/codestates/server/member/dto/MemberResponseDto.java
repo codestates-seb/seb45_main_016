b12b326ebcbbab4ca8bb9e917ba4ff1a6f997c43
luminousol <luminousolding@gmail.com>
2023-08-30 11:54:50 +0900

    feat: MemberResponseDto 추가

---
+++ server/src/main/java/com/codestates/server/member/dto/MemberResponseDto.java
@@ -0,0 +1,27 @@
+package com.codestates.server.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 마이페이지에서 보여질 요소들
+ *
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class MemberResponseDto {
+
+    private String memberId;
+
+    private String email;
+
+    private String nickname;
+
+    private String profileImage;
+
+//    private List<Like> likeList;
+//    private List<Board> boards;
+
+}
