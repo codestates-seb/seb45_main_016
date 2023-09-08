d656333f3c894599e7d9130f7b56e306ae1976d1
luminousol <luminousolding@gmail.com>
2023-09-08 16:10:20 +0900

    comment: 주석추가

--- server/src/main/java/com/codestates/server/global/security/oauth2/v7/CustomOAuth2UserService.java
+++ server/src/main/java/com/codestates/server/global/security/oauth2/v7/CustomOAuth2UserService.java
@@ -25,30 +25,51 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     private final MemberRepository memberRepository;
     private final CustomAuthorityUtils authorityUtils;
 
+    /**
+     * OAuth2UserRequest를 기반으로 OAuth2 사용자 정보 가져오고 처리하는 서비스 클래스
+     *
+     * @param userRequest the user request
+     * @return
+     * @throws OAuth2AuthenticationException
+     */
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
+        // OAuth2UserServicef로 OAuth2 사용자 정보 가지고 오기
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
+        // OAuth2Attribute 클래스로 사용자 정보 추출
         OAuth2Attribute attributes = OAuth2Attribute.of(userRequest, oAuth2User);
 
+        // 추출한 정보로 회원 정보 저장하고 회원 정보 반환
         Member member = saveOAuthUser(attributes);
 
-        return null;
+        // 로그 입력
+        log.info("✨ OAuth User email = {}, nickname = {}" , attributes.getEmail(), attributes.getNickname());
+
+        return CustomOAuth2User.of(member.getMemberId(), attributes);
     }
 
+    /**
+     * OAuth2 사용자 정보를 기반으로 회원 정보 저장하고 반환
+     * @param oAuth2Attribute : OAuth2 사용자 정보 객체
+     * @return 저장된 회원 정보
+     */
     private Member saveOAuthUser(OAuth2Attribute oAuth2Attribute) {
 
+        // 이메일과 제공자(provider)로 가입된 회원 찾기
         Optional<Member> optionalMember
                 = memberRepository.findByEmailAndProvider(oAuth2Attribute.getEmail(), oAuth2Attribute.getProvider());
 
+        // 회원 가입 되어있는지 확인
         if(optionalMember.isPresent()) {
             Member member = optionalMember.orElseThrow(() -> new RuntimeException("🚨 회원 정보를 찾을 수 없습니다. 🚨"));
             return member;
         }
+        // 이미 가입한 회원인지 찾기
         if(memberRepository.existsByEmail(oAuth2Attribute.getEmail())) {
-            throw  new OAuth2AuthenticationException(new OAuth2Error("이미 가입한 회원입니다."), oAuth2Attribute.getEmail());
+            throw  new OAuth2AuthenticationException(new OAuth2Error("🚨 이미 가입한 회원입니다.🚨"), oAuth2Attribute.getEmail());
         }
 
         Member member = new Member();
