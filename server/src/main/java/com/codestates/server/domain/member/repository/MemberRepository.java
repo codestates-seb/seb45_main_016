f93179fc48e3bd8f9cee93c5e4640e30f1fbff1f
luminousol <luminousolding@gmail.com>
2023-09-08 15:44:37 +0900

    feat: findBy 추가

--- server/src/main/java/com/codestates/server/domain/member/repository/MemberRepository.java
+++ server/src/main/java/com/codestates/server/domain/member/repository/MemberRepository.java
@@ -7,4 +7,8 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByEmailAndProvider(String email, String provider);
+
+    boolean existsByEmail(String email);
 }
