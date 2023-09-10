5ede2c6259ea1597ff707274231a2717fcc048ed
luminousol <luminousolding@gmail.com>
2023-09-11 06:59:15 +0900

    feat: annotation 추가

--- server/src/main/java/com/codestates/server/global/security/oauth2/dto/KakaoLoginResponseDto.java
+++ server/src/main/java/com/codestates/server/global/security/oauth2/dto/KakaoLoginResponseDto.java
@@ -2,7 +2,11 @@ package com.codestates.server.global.security.oauth2.dto;
 
 
 import com.codestates.server.domain.member.entity.Member;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class KakaoLoginResponseDto {
 
     public boolean loginSuccess;
