4941476f8415d4b8bdd6b93e0b4633c03dff1880
luminousol <luminousolding@gmail.com>
2023-09-11 05:11:53 +0900

    feat: KakaoLoginResponseDto 추가

---
+++ server/src/main/java/com/codestates/server/global/security/oauth2/dto/KakaoLoginResponseDto.java
@@ -0,0 +1,12 @@
+package com.codestates.server.global.security.oauth2.dto;
+
+
+import com.codestates.server.domain.member.entity.Member;
+
+public class KakaoLoginResponseDto {
+
+    public boolean loginSuccess;
+
+    public Member member;
+
+}
