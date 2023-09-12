f9761bbf3fa1785a760d9984d094753ceb15a349
luminousol <luminousolding@gmail.com>
2023-09-12 20:10:55 +0900

    remove: member entity 수정

--- server/src/main/java/com/codestates/server/domain/member/entity/Member.java
+++ server/src/main/java/com/codestates/server/domain/member/entity/Member.java
@@ -17,20 +17,16 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
-    @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false, unique = true)
     private String name; // 닉네임
 
-    @Column(nullable = false, unique = true)
     private String phone;
 
     private String password;
 
     private String profileImage;
 
-    private String provider;
 
 //    좋아요 리스트에는 자격증 이름, 날짜 다 갖고 있어야 함(날짜는 잘라서 줄 것)
 //    private List<Like> likeList;
@@ -44,16 +40,6 @@ public class Member {
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> roles = new ArrayList<>();
 
-    // ✨ v5,v6
-//    @Builder
-//    public Member(Long memberId, String nickname, String email, String password, String profileImage, String provider, List<String> roles) {
-//        this.memberId = memberId;
-//        this.nickname = nickname;
-//        this.email = email;
-//        this.password = password;
-//        this.profileImage = profileImage;
-//        this.roles = roles;
-//        this.provider = provider;
-//    }
+
 
 }
