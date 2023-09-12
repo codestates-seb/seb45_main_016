6e9fb0d8c12d2fef52c5c8bf0d29742e5bd2f785
luminousol <luminousolding@gmail.com>
2023-09-12 20:10:18 +0900

    chore: 로그 추가

--- server/src/main/java/com/codestates/server/global/security/oauth2/controller/KakaoOAuthController.java
+++ server/src/main/java/com/codestates/server/global/security/oauth2/controller/KakaoOAuthController.java
@@ -4,6 +4,7 @@ import com.codestates.server.domain.member.entity.Member;
 import com.codestates.server.global.security.oauth2.dto.KakaoMemberInfo;
 import com.codestates.server.global.security.oauth2.service.KakaoOAuthService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -55,6 +56,7 @@ import java.util.Map;
 @RestController
 @AllArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@Slf4j
 public class KakaoOAuthController {
 
     private final KakaoOAuthService kakaoOAuthService;
@@ -62,24 +64,34 @@ public class KakaoOAuthController {
     @GetMapping("/login/oauth/code/kakao")
     public ResponseEntity<?> handleKakaoCallback(@RequestParam("code") String code) {
         try {
+            log.info("Received code: {}", code);
             // 1. 프론트에서 받아온 code로 카카오에서 AccessToken 받기
-            String kakaoAccessToken = kakaoOAuthService.exchangeCodeForAccessToekn(code);
+            String kakaoAccessToken = kakaoOAuthService.exchangeCodeForAccessToken(code);
+            log.info("Received Kakao access token: {}", kakaoAccessToken);
 
             // 2. AccessToken으로 사용자 정보 조회
             KakaoMemberInfo kakaoMemberInfo = kakaoOAuthService.getKakaoMemberInfo(kakaoAccessToken);
+            log.info("Kakao member info: {}", kakaoMemberInfo);
 
             // 3. 조회한 사용자 정보 DB에서 조회하기 -> 있으면 로그인, 없으면 저장(가입)
             Member member = kakaoOAuthService.createMemberForKakao(kakaoMemberInfo);
+            log.info("Created/Found member: {}", member);
 
             // 4. 멤버 정보로 토큰 만들기
             String accessToken = kakaoOAuthService.generateAccessToken(member);
+            String refreshToken = kakaoOAuthService.generateRefreshToken(member);
+            log.info("Generated access token: {}", accessToken);
+            log.info("Generated access token: {}", refreshToken);
 
             // 토큰을 응답으로 반환
             Map<String, String> tokenResponse = new HashMap<>();
             tokenResponse.put("accessToken", accessToken);
+            tokenResponse.put("refreshToken", refreshToken);
+            log.info("Returning access token in the response");
 
             return ResponseEntity.ok(tokenResponse);
         } catch (Exception e) {
+            log.error("Error occurred during Kakao OAuth process", e);
             // 예외 발생 시 에러 응답 반환
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
