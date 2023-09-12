acde287868faf7b128877a54053c78e331380647
luminousol <luminousolding@gmail.com>
2023-09-12 09:02:12 +0900

    feat: KakaoOAuthService 수정

--- server/src/main/java/com/codestates/server/global/security/oauth2/service/KakaoOAuthService.java
+++ server/src/main/java/com/codestates/server/global/security/oauth2/service/KakaoOAuthService.java
@@ -1,98 +1,161 @@
 package com.codestates.server.global.security.oauth2.service;
 
-import com.codestates.server.domain.member.dto.MemberResponseDto;
 import com.codestates.server.domain.member.entity.Member;
-import com.codestates.server.domain.member.mapper.MemberMapper;
+import com.codestates.server.domain.member.repository.MemberRepository;
 import com.codestates.server.domain.member.service.MemberService;
+import com.codestates.server.global.security.auth.jwt.JwtTokenizer;
+import com.codestates.server.global.security.auth.utils.CustomAuthorityUtils;
 import com.codestates.server.global.security.oauth2.config.KakaoOAuthConfig;
-import com.codestates.server.global.security.oauth2.dto.KakaoAccountDto;
-import com.codestates.server.global.security.oauth2.dto.KakaoLoginResponseDto;
-import com.codestates.server.global.security.oauth2.dto.KakaoTokenDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.codestates.server.global.security.oauth2.dto.KakaoMemberInfo;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.*;
 
 @AllArgsConstructor
 @Service
+@Transactional
+@Slf4j
 public class KakaoOAuthService {
 
     private final KakaoOAuthConfig kakaoOAuthConfig;
-    private final KakaoMemberService kakaoMemberService;
-    private final MemberMapper mapper;
+    private final MemberRepository memberRepository;
+    private final CustomAuthorityUtils customAuthorityUtils;
+    private final JwtTokenizer jwtTokenizer;
 
 
-    public KakaoTokenDto exchangeCodeForAccessToekn(String code) {
+    /**
+     * 인가 코드로 카카오 토큰 받는 exchangeCodeForAccessToken 메서드
+     * 🔗 https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-token
+     *
+     * @param code
+     * @return
+     */
+    @SneakyThrows
+    public String exchangeCodeForAccessToekn(String code) {
 
         // Http Header 에 Content type 입력 (공식문서 참고)
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        // Http Response Body 생성
+        // Http Request Body 생성
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");   // 고정값
-        body.add("code", code); // 프론트에서 요청 받은 코드 값 (매개변수로 들어오는 값)
         body.add("client_id", kakaoOAuthConfig.getClientId());  // REST API key
         body.add("redirect_uri", kakaoOAuthConfig.getRedirectUri());
+        body.add("code", code); // 프론트에서 요청 받은 코드 값 (매개변수로 들어오는 값)
         body.add("client_secret", kakaoOAuthConfig.getClientSecret());
 
-        // HttpHeader, HttpBody 하나로 담기
+        // HttpRequestHeader, HttpRequestBody 하나로 담아서 요청 보내기
         HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(body, headers);
-
-        // REST API 호출 후 응답을 받을 때 까지 기다리는 동기 방식의 http 템플릿 (json, xml을 쉽게 받음)
+        // RestTemplate : REST API 호출 후 응답을 받을 때 까지 기다리는 동기 방식의 http 템플릿 (json, xml을 쉽게 받음)
         RestTemplate restTemplate = new RestTemplate();
         // 카카오에서 AccessToken 받아오기
-        ResponseEntity<String> kakaoAccessToken = restTemplate.exchange(
+        ResponseEntity<String> response = restTemplate.exchange(
                 "https://kauth.kakao.com/oauth/token",  // token 발급하는 주소
                 HttpMethod.POST,    // POST 요청으로 토큰 발급받음
                 kakaoTokenRequest,
                 String.class
         );
 
-        // JSON 데이터 -> JAVA 데이터로 직렬화
-        // 혹은 JAVA 데이터 -> JSON 데이터로 역직렬화 하는 ObjectMapper 객체 생성
+        // JSON 형태의 kakaoAccessToken -> String으로 parsing
+        String responseBody = response.getBody();
+        // ObjectMapper : JSON 문자열 JAVA 객체로 변환
         ObjectMapper objectMapper = new ObjectMapper();
-        // JavaTimeModule을 ObjectMapper에 등록(날짜, 시간 parsing)
-        objectMapper.registerModule(new JavaTimeModule());
-        // 알 수 없는 JSON속성 예외 반환 하지 않도록 (토큰을 받아야 하기 때문)
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        // "access_token"필드값 추출하고 문자열로 반환
+        return jsonNode.get("access_token").asText();
+    }
 
-        // KakaoTokenDto 초기화 -> JSON 응답 파싱 후 저장
-        KakaoTokenDto kakaoTokenDto = null;
+    /**
+     * 카카오애서 발급받은 accessToken으로 MemberInfo 조회하기
+     * 🔗 https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info-request
+     *
+     * @param accessToken
+     * @return
+     */
+    public KakaoMemberInfo getKakaoMemberInfo(String accessToken) {
 
-        try {
-            // JSON 응답 문자열 objectMapper.readValue() 메서드 사용해서 KakaoTokenDto 객체로 변환
-            kakaoTokenDto = objectMapper.readValue(kakaoAccessToken.getBody(), KakaoTokenDto.class);
-        } catch (JsonProcessingException e) {
-            // JSON 파싱 중에 예외가 발생하면 예외 처리
-            e.printStackTrace();
+        return WebClient.builder()  // WebClient : 비동기 방식으로 원하는 값만 출력할 때 효율적이다
+                .baseUrl("https://kapi.kakao.com/v2/user/me")   // 고정값
+                .build()
+                .get()
+                .header("Authorization", "Bearer " + accessToken)   //
+                .header("Content-type", "application/x-www-form-urlencoded;charset=utf-8")
+                .retrieve()
+                .bodyToMono(KakaoMemberInfo.class)
+                .block();
+    }
+
+    /**
+     * accesstoken으로 조회한 회원 정보로 Member 등록하기
+     *
+     * @param kakaoMemberInfo
+     * @return
+     */
+    public Member createMemberForKakao(KakaoMemberInfo kakaoMemberInfo) {
+
+        String email = kakaoMemberInfo.getEmail();
+
+        // DB에 중복되는 이메일 있는지 확인
+        Optional<Member> existingMember = memberRepository.findByEmail(email);
+
+        // 있으면
+        if (existingMember.isPresent()) {
+            // 그대로 반환
+            return existingMember.get();
+        } else {
+            // 없으면 회원 가입
+            Member newMember = new Member();
+
+            newMember.setName(kakaoMemberInfo.getNickname());
+            newMember.setEmail(kakaoMemberInfo.getEmail());
+            newMember.setProfileImage(kakaoMemberInfo.getProfileImageUrl());
+
+            List<String> roles = customAuthorityUtils.createRoles(newMember.getEmail());
+            newMember.setRoles(roles);
+
+            // 새로운 멤버는 저장하고 리턴
+            return memberRepository.save(newMember);
         }
-        // 파싱된 카카오 토큰 DTO 받아오기
-        return kakaoTokenDto;
     }
 
-    public ResponseEntity<KakaoLoginResponseDto> kakaoLogin(String kakaoAccessToken) {
+    public String generateAccessToken(Member member) {
 
-        // 카카오 계정 정보 가지고 오기
-        KakaoAccountDto kakaoAccountDto = kakaoMemberService.getKakaoInfo(kakaoAccessToken);
+        // jwt 클레임 생성
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("memberId", member.getMemberId());
+        claims.put("email", member.getEmail());
+        claims.put("profileImage", member.getProfileImage());
+        claims.put("roles", member.getRoles());
 
-        // 회원 생성 또는 이미 존재하는 회원 가지고 오기
-        Member member = kakaoMemberService.createMemberForKakao(kakaoAccessToken);
+        log.info("🗝️ JWT 토큰 생성 🗝️");
+        Date expriration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        String accessToken = jwtTokenizer.generateAccessToken(claims, member.getEmail(), expriration, base64EncodedSecretKey);
 
-        // KakaoLoginResponseDto 생성
-        KakaoLoginResponseDto kakaoLoginResponseDto = new KakaoLoginResponseDto();
-        kakaoLoginResponseDto.setLoginSuccess(true);
-        kakaoLoginResponseDto.setMember(member);
-
-//        MemberResponseDto responseDto = mapper.memberToMemberResponseDto(member);
-
-        // 성공적인 로그인 응답 반환
-        return ResponseEntity.ok(kakaoLoginResponseDto);
+        return accessToken;
     }
+
+    public String generateRefreshToken(Member member) {
+
+        log.info("🗝️ JWT 토큰 생성 🗝️");
+        Date expriration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        String refreshToken = jwtTokenizer.generateRefreshToken(member.getEmail(), expriration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+
 }
