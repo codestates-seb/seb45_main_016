4de41f49a74be137f0516ed4609365c084452b80
luminousol <luminousolding@gmail.com>
2023-09-14 12:03:49 +0900

    feat: 파일 업로드 기능 추가

---
+++ server/src/main/java/com/codestates/server/domain/member/dto/MemberImagePatchDto.java
@@ -0,0 +1,12 @@
+package com.codestates.server.domain.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberImagePatchDto {
+
+    private String profileImage;
+
+}
