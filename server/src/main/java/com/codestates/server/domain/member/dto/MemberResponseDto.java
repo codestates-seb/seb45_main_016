ac56e7833aca8be75dd33555d95fe35127c27b40
luminousol <luminousolding@gmail.com>
2023-09-15 20:12:58 +0900

    chore: 마이페이지 responseDto 수정

--- server/src/main/java/com/codestates/server/domain/member/dto/MemberResponseDto.java
+++ server/src/main/java/com/codestates/server/domain/member/dto/MemberResponseDto.java
@@ -1,8 +1,12 @@
 package com.codestates.server.domain.member.dto;
 
+import com.codestates.server.domain.board.entity.Board;
+import com.codestates.server.domain.bookmark.entity.Bookmark;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 /**
  * 마이페이지에서 보여질 요소들
@@ -23,7 +27,8 @@ public class MemberResponseDto {
 
     private String profileImage;
 
-//    private List<Like> likeList;
-//    private List<Board> boards;
+    private List<Bookmark> Bookmarks;
+
+    private List<Board> boards;
 
 }
