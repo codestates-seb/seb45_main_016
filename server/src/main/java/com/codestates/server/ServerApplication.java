7918421f9977efacb4e860f0fec3ede674cd5755
luminousol <luminousolding@gmail.com>
2023-09-17 00:52:58 +0900

    chore: 메일 비동기식 전환(속도문제 해결)

--- server/src/main/java/com/codestates/server/ServerApplication.java
+++ server/src/main/java/com/codestates/server/ServerApplication.java
@@ -3,11 +3,13 @@ package com.codestates.server;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing //스프링 배치 사용
 @EnableScheduling //배치스케줄러 사용
+@EnableAsync	// 비동기 애너테이션
 public class ServerApplication {
 
 	static {
