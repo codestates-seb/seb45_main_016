ead21f8a176d13873760adb7dd7ed06da1c807ed
luminousol <luminousolding@gmail.com>
2023-09-17 00:50:40 +0900

    remove: 안 쓰는 것 제거 및 주석 처리

--- server/src/main/java/com/codestates/server/global/mail/license/CertificateAnnouncementEventHandler.java
+++
@@ -1,9 +0,0 @@
-//package com.codestates.server.global.mail.license;
-//
-///**
-// * 자격증 합격 날짜 알림 핸들러
-// */
-//public class CertificateAnnouncementEventHandler {
-//
-//
-//}
--- server/src/main/java/com/codestates/server/global/mail/license/CertificateExamEventHandler.java
+++
@@ -1,8 +0,0 @@
-//package com.codestates.server.global.mail.license;
-//
-///**
-// * 자격증 시험 날짜 알림 이메일 핸들러
-// */
-//public class CertificateExamEventHandler {
-//
-//}
