318ab6e9135cd13b07ec3dd618b8466f3fa4d05b
kuetiipp <130050839+kuetiipp@users.noreply.github.com>
2023-10-04 16:37:58 +0900

    Revert "feat: comment value clear"

--- client/src/pages/Community/CreateAnswer.js
+++ client/src/pages/Community/CreateAnswer.js
@@ -14,56 +14,38 @@ if (token) {
 
 const CreateAnswer = ({ className, answerAppender }) => {
   const [writeValue, setWriteValue] = useState('');
-  const [isPosted, setPosted] = useState(false);
-  const memberId = localStorage.getItem('memberId');
-  const img = localStorage.getItem('profileImg');
-
 
   const answerPost = (e) => {
-    if (e === 'board-focusing' && isPosted === false) {
+    if (e === 'board-focusing') {
       PostAnswer(writeValue).then(() => answerAppender());
-      setPosted(true);
-    } else if (e === 'focusing-answer' && isPosted === false) {
+    } else if (e === 'focusing-answer') {
       PostComment(writeValue);
-      setPosted(true);
-    }
-  };
-
-  const valueHandler = (e) => {
-    if (isPosted === true) {
-      e.target.value = '';
-      setPosted(false);
-    } else {
-      setWriteValue(e.target.value);
+      console.log(e);
     }
   };
 
   return (
-    <>
-      {memberId ? (
-        <T.CreateAnswerForm>
-          <T.AnswerCratorInfo>
-            <T.CreatorImg src={img} alt="current user img"></T.CreatorImg>
-            <T.Description>
-              <p>{localStorage.getItem('name')}</p>
-            </T.Description>
-          </T.AnswerCratorInfo>
-          <textarea
-            placeholder="댓글을 입력하세요"
-            onChange={valueHandler}
-          ></textarea>
-          <T.AnswerPost
-            className={className}
-            onClick={(e) =>
-              answerPost(e.target.classList[e.target.classList.length - 1])
-            }
-          >
-            등록
-          </T.AnswerPost>
-        </T.CreateAnswerForm>
-      ) : null}
-    </>
-
+    <T.CreateAnswerForm>
+      <T.AnswerCratorInfo>
+        <T.CreatorImg src={img} alt="current user img"></T.CreatorImg>
+        <T.Description>
+          <p>{localStorage.getItem('name')}</p>
+          <p>current user email</p>
+        </T.Description>
+      </T.AnswerCratorInfo>
+      <textarea
+        placeholder="댓글을 입력하세요"
+        onChange={(e) => setWriteValue(e.target.value)}
+      ></textarea>
+      <T.AnswerPost
+        className={className}
+        onClick={(e) =>
+          answerPost(e.target.classList[e.target.classList.length - 1])
+        }
+      >
+        등록
+      </T.AnswerPost>
+    </T.CreateAnswerForm>
   );
 };
 
