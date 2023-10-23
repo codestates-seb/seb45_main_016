4a5ce2dda502d56bb2ba3207ad8d67e43822422a
luminousol <luminousolding@gmail.com>
2023-10-23 14:32:23 +0900

    chore: 오류 수정

--- server/src/main/java/com/codestates/server/global/exception/ExceptionCode.java
+++ server/src/main/java/com/codestates/server/global/exception/ExceptionCode.java
@@ -8,7 +8,7 @@ public enum ExceptionCode {
     //User exists
     USER_EXISTS(409, "User exists"),
     //Unauthorized user
-    UNAUTHORIZED_USER(403, "Unauthorized user"),
+    UNAUTHORIZED_USER(401, "Unauthorized user"),
     // answer not in database
     ANSWER_NOT_FOUND(404, "Answer not found"),
     // comment not in database
@@ -20,7 +20,11 @@ public enum ExceptionCode {
     //Bookmark Not in database
     BOOKMARK_NOT_FOUND(404,"Bookmark not found"),
     //LicenseInfo Not in database
-    LICENSE_NOT_FOUND(404,"License not found");
+    LICENSE_NOT_FOUND(404,"License not found"),
+    // kakako 로부터 유효한 정보 받지 못함
+    UNAUTHORIZED_KAKAO(401, "카카오로부터 유효한 사용자 정보를 받지 못했습니다."),
+    // 이메일이 없는 사용자는 가입할 수 없습니다.
+    UNAUTHORIZED_EMAIL_KAKAO(401, "이메일이 없는 사용자는 가입할 수 없습니다.");
 
 
     @Getter
